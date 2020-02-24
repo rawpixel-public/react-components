611f8ac9c3dca48cc6a4dda9919ce624627b1130
chrishowrawpixel <chris.how.rawpixel@gmail.com>
2020-02-24 17:28:17 +0000

    Removes icons due to weird styled components bug

--- src/components/Sidebar/FilterButtonGroupMain.js
+++ src/components/Sidebar/FilterButtonGroupMain.js
@@ -1,11 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
-
 import Button from "../../atoms/Button";
-import crown from "../../icons/crown.svg";
-import heart from "../../icons/heart-o.svg";
-import { palette } from "../../utils/cssVars";
 
 import { FilterButtonGroupProps } from "./FilterButtonGroup";
 import {
@@ -14,42 +8,11 @@ import {
   StyledWrapper
 } from "./StyledFilterButtonGroup";
 
-const ExclusiveCrown = styled.div`
-  background-color: ${palette.gold};
-  mask: url(${crown}) no-repeat center;
-  text-indent: -9999px;
-  min-height: 20px;
-`;
-
-const ExclusiveButton = props => (
-  <Button {...props} style={{ padding: "5px 0.5rem" }}>
-    <ExclusiveCrown>{props.name}</ExclusiveCrown>
-  </Button>
-);
-
-ExclusiveButton.propTypes = {
-  name: PropTypes.string
-};
-
-const LikesButton = styled(Button)`
-  &:before {
-    content: "";
-    background-color: ${palette.pink};
-    mask: url(${heart}) no-repeat center;
-    min-height: 14px;
-    min-width: 14px;
-    display: inline-block;
-    position: relative;
-    top: 2px;
-    left: -2px;
-  }
-`;
-
 const ButtonComponents = {
   $free: Button,
   $premium: Button,
   $exclusive: Button,
-  $likes: LikesButton
+  $likes: Button
 };
 
 const FilterButtonGroupMain = ({
