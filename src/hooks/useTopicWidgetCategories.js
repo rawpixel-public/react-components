67d4abf4f7fad0e3fdf832f9ba09142b0495ad96
chrishowrawpixel <chris.how.rawpixel@gmail.com>
2020-03-02 12:14:36 +0000

    Adds default object to prevent re-rendering errors

--- src/hooks/useTopicWidgetCategories.js
+++ src/hooks/useTopicWidgetCategories.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-export default (widget = {}) => {
+const defaultWidget = {};
+
+export default (widget = defaultWidget) => {
   const [categories, setCategories] = React.useState([]);
 
   React.useEffect(() => {
