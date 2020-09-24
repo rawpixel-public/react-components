5e0531b53a685657089303461d48ab6fae4c7ca6
chrishowrawpixel <chris.how.rawpixel@gmail.com>
2020-09-24 14:43:53 +0100

    fix: fix issue with duplicate widgets returned from hook

--- src/__tests__/useTopicWidgetsApi.test.js
+++ src/__tests__/useTopicWidgetsApi.test.js
@@ -11,14 +11,36 @@ describe("useTopicWidgets", () => {
   });
 
   test("should call api with expected params", async () => {
-    fetchMocks.mockResponseOnce(async () =>
-      Promise.resolve(JSON.stringify([{ id: 1, title: "My widget" }]))
+    fetchMocks
+      .mockResponseOnce(async () =>
+        Promise.resolve(
+          JSON.stringify([
+            { id: 1, title: "My widget", target: "foo", catalog_id: "bar" }
+          ])
+        )
+      )
+      .mockResponseOnce(async () =>
+        Promise.resolve(
+          JSON.stringify([
+            {
+              id: 2,
+              title: "Another widget",
+              target: "fizz",
+              catalog_id: "buzz"
+            }
+          ])
+        )
+      );
+    const params = { target: "foo", catalog: "bar" };
+    const options = {
+      baseUrl: "https://api.example.com",
+      includeCustom: false
+    };
+    const props = { params, options };
+    const { result, waitForNextUpdate, rerender } = renderHook(
+      props => useTopicWidgetsApi(props.params, props.options),
+      { initialProps: props }
     );
-    const { result, waitForNextUpdate } = renderHook(() => {
-      const params = { target: "foo", catalog: "bar" };
-      const options = { baseUrl: "https://api.example.com" };
-      return useTopicWidgetsApi(params, options);
-    });
 
     expect(result.current.loading).toBe(true);
     expect(Array.isArray(result.current.widgets)).toBe(true);
@@ -36,6 +58,14 @@ describe("useTopicWidgets", () => {
     expect(result.current.loading).toBe(false);
     expect(result.current.widgets.length).toBe(1);
     expect(result.current.widgets[0].title).toBe("My widget");
+
+    const newParams = { target: "fizz", catalog: "buzz" };
+    rerender({ params: newParams, options });
+    expect(result.current.loading).toBe(true);
+    await waitForNextUpdate();
+
+    expect(result.current.widgets.length).toBe(1);
+    expect(result.current.widgets[0].title).toBe("Another widget");
   });
 
   test("should handle errors", async () => {
