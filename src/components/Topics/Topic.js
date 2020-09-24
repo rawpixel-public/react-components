e9bf092eacd48ab412904f7f57b11acd73baacef
chrishowrawpixel <chris.how.rawpixel@gmail.com>
2020-09-24 13:14:36 +0100

    refactor: move hyphenation to custom hook

--- src/components/Topics/Topic.js
+++ src/components/Topics/Topic.js
@@ -11,6 +11,7 @@ import {
   StyledTitleWrapper,
   Mask
 } from "./StyledTopic";
+import useHyphenation from "./useHyphenation";
 
 const TopicIcon = ({ className, icon, name = "" }) => (
   <StyledIconWrapper
@@ -40,52 +41,13 @@ const Topic = ({
   groupIcon,
   ...props
 }) => {
-  const [lines, setLines] = React.useState({ hyphenated: false, multi: false });
-  const TitleRef = React.useRef();
+  const [TitleRef, lines] = useHyphenation(groupIcon, name, active);
 
   const handleTopicClick = e => {
     if (typeof onTopicClick === "function") {
       onTopicClick(e, topic);
     }
   };
-
-  React.useEffect(() => {
-    if (!name || !("createRange" in document)) {
-      return;
-    }
-
-    const LINE_HEIGHT = 15;
-    const titleElement = TitleRef.current;
-    const textNode = Array.from(titleElement.childNodes).find(
-      e => e.nodeName === "#text"
-    );
-    const text = name;
-
-    // Check if any words' bounding rect spans more than one line.
-    const words = text.split(" ");
-    const wordSpansLine = !!words
-      .map(word => {
-        const range = document.createRange();
-        const start = text.indexOf(word);
-        const end = start + word.length;
-        range.setStart(textNode, start);
-        range.setEnd(textNode, end);
-        return range.getBoundingClientRect().height > LINE_HEIGHT;
-      })
-      .filter(Boolean).length;
-
-    // Apply hyphenated css if a word spans across lines.
-    if (wordSpansLine) {
-      setLines({ hyphenated: true, multi: true });
-    } else if (words.length === 1 && titleElement.clientHeight > LINE_HEIGHT) {
-      setLines({ hyphenated: true, multi: true });
-    } else {
-      setLines({
-        hyphenated: false,
-        multi: titleElement.clientHeight > LINE_HEIGHT
-      });
-    }
-  }, [groupIcon, name]);
 
   return (
     <StyledTopicButton
