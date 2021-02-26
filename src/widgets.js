bae5935db311520c092abf4f4802e59d1f153ecf
chrishowrawpixel <chris.how.rawpixel@gmail.com>
2021-02-26 16:42:34 +0000

    feat: split widgets components into separate bundle

---
+++ src/widgets.js
@@ -0,0 +1,5 @@
+import WidgetTopicGroup from "./components/Widgets/TopicGroup";
+import WidgetAddon from "./components/Widgets/Addon";
+import WidgetsBar from "./components/Widgets/Bar";
+
+export { WidgetsBar, WidgetAddon, WidgetTopicGroup };
