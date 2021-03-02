2ac71bf4136357c4a76f1be6fa90e9fcedaa94ff
chrishowrawpixel <chris.how.rawpixel@gmail.com>
2021-03-02 09:17:11 +0000

    refactor: split exports

---
+++ src/hooks.js
@@ -0,0 +1,5 @@
+import useTopicsApi from "./hooks/useTopicsApi";
+import useTopicsFiltersApi from "./hooks/useTopicsFiltersApi";
+import useTopicWidgetsApi from "./hooks/useTopicWidgetsApi";
+
+export { useTopicsApi, useTopicsFiltersApi, useTopicWidgetsApi };
