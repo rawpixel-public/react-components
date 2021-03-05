9b94c77ec58bdf97ab4c9751854924cf164ed8f3
chrishowrawpixel <chris.how.rawpixel@gmail.com>
2021-03-05 16:06:16 +0000

    fix: update size button background

--- src/atoms/SizeButton/StyledSizeButton.js
+++ src/atoms/SizeButton/StyledSizeButton.js
@@ -2,54 +2,6 @@ import styled, { css } from "styled-components";
 import { ifProp } from "styled-tools";
 import { fontFamily, palette } from "../../utils/cssVars";
 
-export const StyledSizeButton = styled.button`
-  background: none;
-  border: none;
-  max-width: 60px;
-  padding-bottom: 5px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-content: center;
-  align-items: center;
-  text-decoration: none;
-  color: #7d7c7c;
-
-  @media (hover: hover) {
-    &:hover {
-      cursor: pointer;
-    }
-
-    &:hover .block {
-      opacity: 0.8;
-    }
-
-    &:not([disabled]):hover > div > div {
-      background: #4b4b4c;
-    }
-
-    &:not([disabled]):hover > span {
-      color: #4b4b4c;
-    }
-
-    &[disabled]:hover {
-      cursor: not-allowed;
-    }
-  }
-
-  ${ifProp(
-    "active",
-    css`
-      & > div > div {
-        background: #4b4b4c;
-      }
-      & > span {
-        color: #4b4b4c;
-      }
-    `
-  )}
-`;
-
 export const StyledBlock = styled.div`
   background: ${palette.grayLight};
   border-radius: 3px;
@@ -72,4 +24,52 @@ export const BlockWrapper = styled.div`
   justify-content: center;
   height: 50px;
   width: 60px;
+`;
+
+export const StyledSizeButton = styled.button`
+  background: none;
+  border: none;
+  max-width: 60px;
+  padding-bottom: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  text-decoration: none;
+  color: #7d7c7c;
+
+  @media (hover: hover) {
+    &:hover {
+      cursor: pointer;
+    }
+
+    &:hover ${BlockWrapper} {
+      opacity: 0.8;
+    }
+
+    &:not([disabled]):hover ${StyledBlock} {
+      background: #4b4b4c;
+    }
+
+    &:not([disabled]):hover ${StyledTitle} {
+      color: #4b4b4c;
+    }
+
+    &[disabled]:hover {
+      cursor: not-allowed;
+    }
+  }
+
+  ${ifProp(
+    "active",
+    css`
+      ${StyledBlock} {
+        background: ${palette.fuscous};
+      }
+      ${StyledTitle} {
+        color: ${palette.fuscous};
+      }
+    `
+  )}
 `;
